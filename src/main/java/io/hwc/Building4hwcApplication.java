3b08955031892982dbda632c63eb8de7c1ce99e4
FanJups <jupsfan@gmail.com>
2019-12-17 12:30:54 +0100

    Creating Spring Boot Project

---
+++ src/main/java/io/hwc/Building4hwcApplication.java
@@ -0,0 +1,13 @@
+package io.hwc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Building4hwcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Building4hwcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/hwc/Building4hwcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hwc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Building4hwcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
